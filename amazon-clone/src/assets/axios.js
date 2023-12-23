18e503ae9593a8b59d4cc8a1ace42dceab21f98f
lalisa-1 <lelisawak@gmail.com>
2023-12-22 20:29:20 -0600

    cyclic URL updated

--- amazon-clone/src/assets/axios.js
+++ amazon-clone/src/assets/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
 	// THE API (cloud function) URL
-	baseURL: "http://localhost:10000/",
+	// baseURL: "http://localhost:10000/",
+	baseURL: "https://amazon-clone-by-lalisa.cyclic.app",
 });
 
 export default instance;
